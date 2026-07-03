aa4006a0877317bf76adf4001871b2fdaac3edcb
Cee-Ry <eduaveceasar@gmail.com>
2026-07-03 21:42:42 +0800

    feat: scope line

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -85,4 +85,18 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "BufReadPost",
+    config = function()
+      require("ibl").setup({
+        scope = {
+          char = "│",
+          show_start = true,
+          show_end = true,
+        },
+      })
+    end,
+  },
 })
