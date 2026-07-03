8b15c1d18f5d3a0d4178f97b5b16bb5428233584
Cee-Ry <eduaveceasar@gmail.com>
2026-07-03 16:35:42 +0800

    feat: ./lua/custom/options.lua

---
+++ lua/custom/options.lua
@@ -0,0 +1,41 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+  
+local set = vim.opt
+
+-- Line Numbers 
+set.number = true
+set.relativenumber = false
+
+-- Tab & Indentation
+set.tabstop = 2                -- Number of spaces a tab counts for
+set.shiftwidth = 2             -- Number of spaces for auto-indent
+set.softtabstop = 2            -- Number of spaces for a tab key press
+set.expandtab = true           -- Convert tabs to spaces
+set.smartindent = true         -- Smart auto-indenting
+set.autoindent = true
+
+-- Search Behavior
+set.ignorecase = true          -- Case-insensitive search
+set.smartcase = false           -- Case-sensitive if uppercase used
+set.hlsearch = true            -- Highlight search matches
+set.incsearch = true           -- Show matches as you type
+
+-- Visual & UI
+set.termguicolors = true       -- Enable true colors (24-bit)
+set.cursorline = true          -- Highlight current line
+set.signcolumn = "yes"         -- Always show sign column (for git/lint icons)
+set.scrolloff = 8              -- Keep 8 lines above/below cursor
+set.sidescrolloff = 8          -- Keep 8 columns left/right of cursor
+set.wrap = false               -- Don't wrap long lines
+set.showmode = false           -- Don't show mode in command line (statusline handles it)
+
+-- 6. Clipboard & System
+set.clipboard = "unnamedplus"  -- Sync with system clipboard
+
+-- 8. Split Windows
+set.splitright = true          -- Vertical split goes to the right
+set.splitbelow = true          -- Horizontal split goes below
+
+-- 9. Mouse Support
+set.mouse = "a"                -- Enable mouse in all modes
