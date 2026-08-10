7bf240439ec39f9c86a9e51a915e72d8bcf66f00
Cee-Ry <eduaveceasar@gmail.com>
2026-08-10 21:35:29 +0800

    feat: split editor

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -3,6 +3,9 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>n", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
 vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<CR>", { desc = "Open vertical split" })
+vim.keymap.set("n", "<leader>V", "<cmd>vsplit<CR>", { desc = "Open vertical split" })
+vim.keymap.set("n", "<leader>w", "<C-w>w", { desc = "Switch window focus" })
 vim.keymap.set("n", "<leader>t", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 vim.keymap.set("n", "<A-Tab>", "<cmd>tabnext<CR>", { desc = "Next tab" })
 vim.keymap.set("n", "<A-S-Tab>", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
